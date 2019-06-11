254848b3b449ce9b1fa40a1e25c33f344bb01f40
Adriano Mota <adrianowsh@hotmail.com>
2019-06-10 23:25:54 -0300

    first commit

---
+++ src/pages/Main.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Logo from '../componenys/Logo';
+
+const Main = () => <Logo />;
+
+export default Main;
---
+++ src/store/reducers/repos.js
@@ -0,0 +1,9 @@
+const INITIAL_STATE = {
+  repo: [],
+};
+export default function repos(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
